e278152b5c214fd3afb49157bd1b50d3e3adc4a5
siqeknn123 <siqeknn@gmail.com>
2025-08-31 13:29:00 +0300

    add maps

--- map_manager.py
+++ map_manager.py
@@ -9,7 +9,6 @@ class MapManager:
             (255, 242, 0, 0.6)
         ]
         self.add_land_node()
-        self.add_block((1, 1, 1))
 
     def add_land_node(self):
         self.land = render.attachNewNode("Land")
@@ -18,9 +17,29 @@ class MapManager:
         self.land.removeNode()
         self.add_land_node()
 
+    def set_color(self, z: int):
+        if z <= 3:
+            return self.colors[z]
+        else:
+            return self.colors[0]
+
     def add_block(self, possition: tuple) -> None:
         self.block = loader.loadModel(self.model)
-        self.block.setTexture(loader.loadTexture(self.texture))        
-        self.block.setColor(self.colors[0])
+        self.block.setTexture(loader.loadTexture(self.texture))     
+        color = self.set_color(possition[2])   
+        self.block.setColor(color)
         self.block.setPos(possition)
         self.block.reparentTo(self.land)
+
+    def load_map(self, filename):
+        with open(filename) as file:
+            y = 0
+            for line in file:
+                x = 0
+                line_lst = line.split(" ")
+                for z in line_lst:
+                    for z0 in range(int(z) + 1):
+                        block = self.add_block((x, y, z0))
+                    x += 1
+                y += 1
+        return x, y
