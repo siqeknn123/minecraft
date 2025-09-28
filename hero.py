1d7c8666e1bb9a56b7da115d4ad90a94b1b43df4
siqeknn123 <siqeknn@gmail.com>
2025-09-28 13:32:31 +0300

    add, remove block, save, load map

--- hero.py
+++ hero.py
@@ -1,60 +1,72 @@
 class Hero:
-    def __init__(self, pos, land):
-        self.camera_mode = None
-        self.game_mode = True
-        self.land = land
-        self.hero = loader.loadModel("smiley")
-        self.hero.setColor((252 // 100, 255 // 100, 0 // 100, 0.8))
-        self.hero.setScale(0.3)
-        self.hero.setPos(pos)
-        self.hero.reparentTo(render)
-
-        self.camera_bind()
-        self.accept_events()
-
+    def __init__(self, position, land):
+            self.camera_mode = None
+            self.game_mode = True
+            self.land = land
+            self.hero = loader.loadModel('smiley')
+            self.hero.setColor((255//100, 0//100, 0//100, 1))
+            self.hero.setScale(0.5)
+            self.hero.setPos(position)
+            self.hero.reparentTo(render)
+            
+            self.camera_bind()
+            self.accept_events()    
     def camera_bind(self):
         base.disableMouse()
         base.camera.setH(180)
         base.camera.reparentTo(self.hero)
         base.camera.setPos(0, 0, 1.5)
         self.camera_mode = True
-
-    def cameraUp(self):
+        
+    def camera_up(self):
         pos = self.hero.getPos()
-        base.mouseInterfaceNode.setPos(-pos[0], -pos[1], pos[2]-3)
+        base.mouseInterfaceNode.setPos(-pos[0], -pos[1], -pos[2] - 3)
         base.camera.reparentTo(render)
         base.enableMouse()
         self.camera_mode = False
-    
-    def switch_cam(self):
+    def switch_camera(self):
         if self.camera_mode:
-            self.cameraUp()
+            self.camera_up()
         else:
             self.camera_bind()
-
+            
+    def change_mode(self):
+        if self.game_mode:
+            self.game_mode = False  
+        else:
+            self.game_mode = True
+            
     def turn_left(self):
-        self.hero.setH((self.hero.getH() + 5) % 360)
-    
+        self.hero.setH(self.hero.getH() + 5 % 360)
+        
     def turn_right(self):
-        self.hero.setH((self.hero.getH() - 5) % 360)
-
+        self.hero.setH(self.hero.getH() - 5 % 360) 
+        
     def move_to(self, angle):
-        """Обираємо як рухати гравця в залежності від режиму гри"""
+        """"обираємо як рухати гравця в залежності від напрямку""" 
         if self.game_mode:
             self.just_move(angle)
         else:
             self.try_move(angle)
-        
-    def just_move(self, angle):
-        """"Рух гравця в режимі спостерігача"""
+    def just_move(self,angle):
+        """"рухаємо гравця вперед""" 
         pos = self.look_at(angle)
         self.hero.setPos(pos)
-
         
-    def try_move(self):
-        """"Рух гравця в режимі ігровому режимі"""
-        pass
-
+        
+    
+    def try_move(self,angle):
+        """"рухаємо гравця по ігровому режиму""" 
+        pos = self.look_at(angle)
+        if self.look_at(angle):
+            if self.land.is_empty(pos):
+                pos = self.land.find_highest(pos)
+                self.hero.setPos(pos)
+        else:
+            pos = pos[0], pos[1], pos[2] + 1
+            if self.land.is_empty(pos):
+                self.hero.setPos(pos)
+        
     def check_dir(self, angle):
        ''' повертає заокруглені зміни координат X, Y,
        відповідні переміщенню у бік кута angle.
@@ -90,51 +102,79 @@ class Hero:
            return -1, -1
        else:
            return 0, -1
-
-    def look_at(self, angle):
+       
+    def look_at(self,angle):
         x = round(self.hero.getX())
         y = round(self.hero.getY())
         z = round(self.hero.getZ())
-
+        
         dx, dy = self.check_dir(angle)
-
+        
         return x + dx, y + dy, z
-
+    
     def forward(self):
         angle = self.hero.getH() % 360
-        self.move_to(angle) 
-
-    def left(self):
-        angle = (self.hero.getH() - 90) % 360    
-        self.move_to(angle) 
-
-    def right(self):
-        angle = (self.hero.getH() + 90) % 360    
-        self.move_to(angle) 
-    
-    def back(self):
-        angle = (self.hero.getH() - 180) % 360    
         self.move_to(angle)
-
+        
+    def left(self):
+        angle = (self.hero.getH() + 90) % 360
+        self.move_to(angle)
+   
+    def right(self):
+        angle = (self.hero.getH() - 90) % 360
+        self.move_to(angle)
+        
+    def back(self):
+        angle = (self.hero.getH() + 180) % 360
+        self.move_to(angle)
+        
+        
     def up(self):
-        if self.game_mode:
-            self.hero.setZ(self.hero.getZ() + 1)
-
+        self.hero.setZ(self.hero.getZ() + 1)
+        
     def down(self):
         if self.game_mode:
             self.hero.setZ(self.hero.getZ() - 1)
+         
+    def build(self):
+        angle = self.hero.getH() % 360
+        pos = self.look_at(angle)
+        if self.game_mode:
+            self.land.add_block(pos)
+        else:
+            self.land.build_block(pos)
+            
+    def destroy(self):
+        angle = self.hero.getH() % 360
+        pos = self.look_at(angle)
+        if self.game_mode:
+            self.land.destroy_block(pos)
+        else:
+            self.land.del_block_from(pos)
+       
 
-    
-
+        
+        
     def accept_events(self):
-        base.accept("c", self.switch_cam)
-        base.accept("a", self.turn_left)
-        base.accept("a" + "-repeat", self.turn_left)
-        base.accept("d", self.turn_right)
-        base.accept("d" + "-repeat", self.turn_right)
-        base.accept("w" + "-repeat", self.forward)
-        base.accept("s" + "-repeat", self.back)
-        base.accept("a", self.right)
-        base.accept("d", self.left)
-        base.accept("q", self.down)
-        base.accept("e", self.up)
+        base.accept('c', self.switch_camera)
+        base.accept('a', self.turn_left)
+        base.accept('a''-repeat', self.turn_left)
+        base.accept('d', self.turn_right)
+        base.accept('d''-repeat', self.turn_right)
+        base.accept('w', self.forward)
+        base.accept('w''-repeat', self.forward) 
+        base.accept('s', self.back)
+        base.accept('s''-repeat', self.back)
+        base.accept('q', self.left)
+        base.accept('q''-repeat', self.left)  
+        base.accept('e', self.right)
+        base.accept('e''-repeat', self.right)
+        base.accept('r', self.up)
+        base.accept('r''-repeat', self.up)
+        base.accept('f', self.down)
+        base.accept('f''-repeat', self.down)
+        base.accept('z', self.change_mode)
+        base.accept('mouse1', self.build)
+        base.accept('v', self.destroy)
+        base.accept('o', self.land.load_map_from_file)
+        base.accept('p', self.land.save_map)
--- map_manager.py
+++ map_manager.py
@@ -1,37 +1,41 @@
+import pickle
 class MapManager:
     def __init__(self):
-        self.model = "models/block.egg"
-        self.texture = "textures/custom1.png"
+        self.model = 'models/block.egg'
+        self.texture = 'textures/brick.png'
         self.colors = [
-            (255, 0, 132, 0.6),
-            (82, 255, 0, 0.85),
-            (0, 13, 255, 0.85),
-            (255, 242, 0, 0.6)
+            (132, 195, 204, 1),
+            (202, 59, 253,1),
+            (0, 255, 127,1),
+            (135, 206, 250,1),
+            
         ]
         self.add_land_node()
-
+        self.add_block((1,1,1))
+        
     def add_land_node(self):
-        self.land = render.attachNewNode("Land")
-
-    def clear_land_node(self):
+        self.land = render.attachNewNode('Land')
+    
+    def clear_land(self):
         self.land.removeNode()
         self.add_land_node()
-
-    def set_color(self, z: int):
+        
+    def set_color(self, z:int):
         if z <= 3:
             return self.colors[z]
         else:
             return self.colors[0]
-
-    def add_block(self, possition: tuple) -> None:
+    
+    def add_block(self, position:int):
         self.block = loader.loadModel(self.model)
-        self.block.setTexture(loader.loadTexture(self.texture))     
-        color = self.set_color(possition[2])   
+        self.block.setTexture(loader.loadTexture(self.texture))
+        color = self.set_color(position[2])
         self.block.setColor(color)
-        self.block.setPos(possition)
+        self.block.setPos(position)
+        self.block.setTag("att",str(position))
         self.block.reparentTo(self.land)
 
-    def load_map(self, filename):
+    def load_map(self,filename):
         with open(filename) as file:
             y = 0
             for line in file:
@@ -39,7 +43,68 @@ class MapManager:
                 line_lst = line.split(" ")
                 for z in line_lst:
                     for z0 in range(int(z) + 1):
-                        block = self.add_block((x, y, z0))
+                        block = self.add_block((x,y, z0))
                     x += 1
                 y += 1
         return x, y
+    
+    def find_blocks(self, pos):
+        """шукаємо блок по позиції"""
+        return self.land.findAllMatches("=at=" + str(pos))
+
+    def is_empty(self, pos):
+        """перевіряємо чи є блок на позиції"""
+        if self.find_blocks(pos):
+            return False
+        else:
+            return True
+        
+    def find_highest(self, pos):
+        """шукаємо найвищий блок на позиції (x,y)"""
+        x,y,z = pos
+        z = 1 
+        while not self.is_empty((x,y,z)):
+            z += 1
+        return (x,y,z)
+    
+    def build_block(self, pos):
+        """будуємо блок на позиції"""
+        x, y, z = pos
+        new = self.find_highest(pos)
+        print(new)
+        if new[2] <= z + 1:
+            self.add_block(new)
+
+    def destroy_block(self, pos):
+        """видаляємо блок на позиції"""
+        blocks = self.find_blocks(pos)
+        for blok in blocks:
+            blok.removeNode()
+
+    def del_block_from(self, pos):
+        x, y, z = self.find_highest(pos)
+        pos = x, y, z - 1
+        blocks = self.find_blocks(pos)
+        for block in blocks:
+            block.removeNode()
+
+
+    def save_map(self):
+        blocks = self.land.getChildren()
+        with open("my_map.dat", "wb") as file:
+            pickle.dump(len(blocks), file)
+            for block in blocks:
+                x,y,z = block.getPos()
+                pos = (int(x), int(y), int(z))
+                pickle.dump(pos, file)
+
+    def load_map_from_file(self):
+        with open("my_map.dat", "rb") as file:
+            lenght = pickle.load(file)
+            for i in range(lenght):
+                pos = pickle.load(file)
+                self.add_block(pos)
+
+
+
+        
